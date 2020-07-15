7b49f57a1090742467651631481fb2d61546d219
Alex <ajbarbati@gmail.com>
2020-07-15 17:36:08 -0400

    Updates

---
+++ precache-manifest.107b6925b553d759090242f96511a840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faf2d84935eb398a6d2c3b4b1006640",
+    "url": "/portfolio_site/index.html"
+  },
+  {
+    "revision": "389130b1290ce639caad",
+    "url": "/portfolio_site/static/css/main.6c61cc7b.chunk.css"
+  },
+  {
+    "revision": "52cbfb2d16a70b9121cd",
+    "url": "/portfolio_site/static/js/2.fcede9d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_site/static/js/2.fcede9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389130b1290ce639caad",
+    "url": "/portfolio_site/static/js/main.6bc25f2f.chunk.js"
+  },
+  {
+    "revision": "a95471b8837da813bf19",
+    "url": "/portfolio_site/static/js/runtime-main.830e3382.js"
+  }
+]);
