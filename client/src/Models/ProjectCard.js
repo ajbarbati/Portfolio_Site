9b490ae0887d09ecf2df5b7b068c2bc02af5ed64
Alex <ajbarbati@gmail.com>
2020-10-23 14:24:31 -0400

    slight change to fix warnings

--- client/src/Models/ProjectCard.js
+++ client/src/Models/ProjectCard.js
@@ -64,7 +64,7 @@ onFilterChange(filter) {
   }
 
   render() {
-    const { techSkills, activeFilter, colors } = this.state;
+    const { techSkills, activeFilter} = this.state;
     let filteredList;
     if (
       activeFilter.length === 0 ||
@@ -126,10 +126,10 @@ onFilterChange(filter) {
                 <h2 className="projectName">{project.Name}</h2>
                 <div className="projectLinks">
                   <a className="projectLink A" href={project.URL}>
-                    <img className="pGitLink" src="https://res.cloudinary.com/dz3p8xcr8/image/upload/v1594343230/Portfolio/github-1_nqhu66.png"></img>
+                    <img className="pGitLink" alt="github" src="https://res.cloudinary.com/dz3p8xcr8/image/upload/v1594343230/Portfolio/github-1_nqhu66.png"></img>
                   </a>
                   <a className="projectLink B" href={project.URL}>
-                    <img className="pHostLink" src="https://res.cloudinary.com/dz3p8xcr8/image/upload/v1594343347/Portfolio/Icon_awesome-link_qwksft.png"></img>
+                    <img className="pHostLink" alt="hosted project" src="https://res.cloudinary.com/dz3p8xcr8/image/upload/v1594343347/Portfolio/Icon_awesome-link_qwksft.png"></img>
                   </a>
                 </div>
                 <div className="pImgContainer">
@@ -154,34 +154,3 @@ onFilterChange(filter) {
 }
 
 export default ProjectCard
-{
-  /* HERES MORE PROJECTS
-        
-        
-       ,
-        {
-            "Name": "Data Dashboard",
-            "Image": "Images4",
-            "Tech Used": [
-                "React",
-                "Node.js",
-                "Chart.js",
-                "MySQL"
-            ],
-            "Description": "A data dashboard and or Admin Panel, with Node.js for Login and Registering accounts."
-        },
-        {
-            "Name": "NEED TO MAKE A BIG 5th Project",
-            "Image": "Images4",
-            "Tech Used": [
-                "Node.js",
-                "CSS",
-                "Handlebars",
-                "MailJet API"
-            ],
-            "Description": "This Is Project 4"
-        }
-
-        
-        */
-}
